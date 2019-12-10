d4e70c9b7ee9bda47320aafe96003272c522796c
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-12-10 18:06:19 +0530

    dashboard start

---
+++ src/MeterDataDashboard.Web/Controllers/FictDataController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using MeterDataDashboard.Application;
+using MeterDataDashboard.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace MeterDataDashboard.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FictDataController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public FictDataController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{tag}/{start_date}/{end_date}")]
+        public IEnumerable<double> Index(string tag, string start_date, string end_date)
+        {
+            // https://localhost:44390/api/fictdata/LO-91/2018-05-01/2018-05-10
+            List<double> res = new List<double>();
+            DateTime startDate = DateTime.ParseExact(start_date, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(end_date, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            try
+            {
+                // Connect to a PostgreSQL database
+                NpgsqlConnection conn = new NpgsqlConnection(_configuration["ConnectionStrings:MeterConnection"]);
+                conn.Open();
+
+                // Define a query
+                NpgsqlCommand command = new NpgsqlCommand(@"SELECT data_time, mwh FROM public.fict_location_energy_data 
+                                                            where location_id=@locationId and data_time 
+                                                            between @startTime and @endTime order by data_time", conn);
+
+                command.Parameters.AddWithValue("@locationId", tag);
+                command.Parameters.AddWithValue("@startTime", startDate);
+                command.Parameters.AddWithValue("@endTime", endDate);
+
+                // Execute the query and obtain a result set
+                NpgsqlDataReader dr = command.ExecuteReader();
+
+                while (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        DateTime dt = dr.GetDateTime(0);
+                        double ts = TimeUtils.ToMillisSinceUnixEpoch(dt);
+                        double val = dr.GetDouble(1);
+                        res.Add(ts);
+                        res.Add(val);
+                    }
+                    dr.NextResult();
+                }
+
+                dr.Dispose();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return res;
+        }
+    }
+}
---
+++ src/MeterDataDashboard.Web/Models/FictDataFetchFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeterDataDashboard.Web.Models
+{
+    public class FictDataFetchFormModel
+    {
+        [Required]
+        public DateTime LocationTag { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+    }
+}

5936c3aef31d4a65d3143fc78d456e781f8c95f2
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-12-12 09:43:25 +0530

    added plotly dist as external module to reduce webpack build time

--- src/MeterDataDashboard.Web/wwwroot/js/fict-dash-app/webpack.config.js
+++ src/MeterDataDashboard.Web/wwwroot/js/fict-dash-app/webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
         filename: "main.js"
     },
 
+    externals: {
+        'plotly.js-dist': 'Plotly'
+    },
+
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 

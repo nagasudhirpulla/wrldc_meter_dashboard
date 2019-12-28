005b6c13677ac8227dfe334ae98a472bee64e41b
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-12-28 12:42:45 +0530

    extension for signing cert

---
+++ src/MeterDataDashboard.Infra/IdentityServer/Extensions/IdentityServerBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeterDataDashboard.Infra.IdentityServer.Extensions
+{
+    public static class IdentityServerBuilderExtensions
+    {
+        // this extension class code is taken from https://kimsereyblog.blogspot.com/2018/07/self-signed-certificate-for-identity.html
+        // guide to generate certificate using openssl - https://benjii.me/2017/06/creating-self-signed-certificate-identity-server-azure/
+        // download openssl from https://slproweb.com/products/Win32OpenSSL.html
+        // openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout example.key -out example.crt -subj "/CN=example.com" -days 3650
+        // openssl pkcs12 -export -out example.pfx -inkey example.key -in example.crt -certfile example.crt
+        // use certlm.msc to manage certificates and trusted certificates - http://woshub.com/how-to-create-self-signed-certificate-with-powershell/
+        public static IIdentityServerBuilder LoadSigningCredentialFrom(this IIdentityServerBuilder builder, string certName)
+        {
+            if (!string.IsNullOrWhiteSpace(certName))
+            {
+                builder.AddSigningCredential(certName);
+            }
+            else
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+
+            return builder;
+        }
+    }
+}

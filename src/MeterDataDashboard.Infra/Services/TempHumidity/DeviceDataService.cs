e1bf00f59ca4ffe819bf195381d748437f3b7290
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2020-06-30 13:25:11 +0530

    changes

--- src/MeterDataDashboard.Infra/Services/TempHumidity/DeviceDataService.cs
+++ src/MeterDataDashboard.Infra/Services/TempHumidity/DeviceDataService.cs
@@ -11,17 +11,18 @@ namespace MeterDataDashboard.Infra.Services.TempHumidity
         public async Task<List<DeviceDataDTO>> GetRealTimeDevicesData()
         {
             List<DeviceDataDTO> res = new List<DeviceDataDTO>();
-            List<(string name, string ip, int port)> devices = new List<(string name, string ip, int port)>()
+            //List<(string name, string ip, int port)> devices = new List<(string name, string ip, int port)>()
+            //{
+            //    ("Server Room", "10.2.100.72", 4567),
+            //    ("Communication Room", "10.2.100.73", 4567),
+            //    ("UPS Room", "10.2.100.74", 4567)
+            //};
+            List<(string name, string ip, int port)> devices = new List<(string name, string ip, int port)>();
+            foreach ((string name, string ip, int port) in devices)
             {
-                ("Server Room", "10.2.100.72", 4567),
-                ("Communication Room", "10.2.100.73", 4567),
-                ("UPS Room", "10.2.100.74", 4567)
-            };
-            foreach (var dev in devices)
-            {
-                double? devTemp = DeviceDataFetcher.FetchData(dev.ip, DataType.Temperature, dev.port);
-                double? devHum = DeviceDataFetcher.FetchData(dev.ip, DataType.Humidity, dev.port);
-                res.Add(new DeviceDataDTO() { Humidity = devHum, Tempurature = devTemp, DeviceName = dev.name });
+                double? devTemp = DeviceDataFetcher.FetchData(ip, DataType.Temperature, port);
+                double? devHum = DeviceDataFetcher.FetchData(ip, DataType.Humidity, port);
+                res.Add(new DeviceDataDTO() { Humidity = devHum, Tempurature = devTemp, DeviceName = name });
             }
             return await Task.FromResult(res);
         }
